c25128ec3ab76d4ab639e6f1f78348f821f3fbd1
Patrick Whelan <patrickwhelan@gmx.com>
2021-01-12 13:49:06 +0000

    Solution

--- src/RomanNumeral.php
+++ src/RomanNumeral.php
@@ -4,13 +4,14 @@ namespace PhpNwSykes;
 class RomanNumeral
 {
     protected $symbols = [
-        1000 => 'M',
-        500 => 'D',
-        100 => 'C',
-        50 => 'L',
-        10 => 'X',
-        5 => 'V',
-        1 => 'I',
+
+	'M' => 1000,
+	'D' => 500,
+	'C' => 100,
+	'L' => 50,
+	'X' => 10,
+	'V' => 5,
+	'I' => 1,
     ];
 
     protected $numeral;
@@ -27,8 +28,32 @@ class RomanNumeral
      */
     public function toInt():int
     {
-        $total = 0;
+	$total = 0;
+	$numLen = strlen($this->numeral);
 
-        return $total;
+	try
+	{
+		// throw if string is empty or last char invalid numeral
+		if($numLen < 1 || !array_key_exists($this->numeral[$numLen-1], $this->symbols))
+			throw new InvalidNumeral();
+
+	        $total = $this->symbols[$this->numeral[$numLen-1]];
+
+		for ($i = $numLen-2; $i >= 0 ; --$i)
+		{
+			// throw if char is invalid numeral
+			if(!array_key_exists($this->numeral[$i], $this->symbols))
+				throw new InvalidNumeral();
+
+			// if out of descending order (front to back) then subtract
+			if($this->symbols[$this->numeral[$i]] < $this->symbols[$this->numeral[$i+1]])
+				$total -= $this->symbols[$this->numeral[$i]];
+			else
+				$total += $this->symbols[$this->numeral[$i]];
+		}
+
+    	}catch(Exception $e){echo $e->getMessage();}
+
+	return $total;
     }
 }
